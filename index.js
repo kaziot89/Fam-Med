d96101ffd859aa52eaa1d0b38868cf27f3bf5e40
Kazimierz Targowski <k.targowski@gmail.com>
2024-10-21 18:26:17 +0200

    hamburger and some css fixes

--- index.js
+++ index.js
@@ -244,3 +244,41 @@ menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("open");
   navMenu.classList.toggle("activeH");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const infosButton = document.getElementById("infos_btn");
+  const infosDropdown = document.getElementById("infos_dropdown");
+  const placesButton = document.getElementById("places_btn");
+  const placesDropdown = document.getElementById("places_dropdown");
+
+  let isAnimating = false; // Zmienna do śledzenia animacji
+
+  function closeDropdown(dropdown, callback) {
+    if (dropdown.classList.contains("open")) {
+      isAnimating = true; // Ustawiamy animację na true
+      dropdown.classList.remove("open");
+      setTimeout(() => {
+        isAnimating = false; // Po zakończeniu animacji ustawiamy na false
+        callback();
+      }, 300); // Ustaw czas zgodny z transition
+    } else {
+      callback();
+    }
+  }
+
+  infosButton.addEventListener("click", () => {
+    if (!isAnimating) {
+      closeDropdown(placesDropdown, () => {
+        infosDropdown.classList.toggle("open");
+      });
+    }
+  });
+
+  placesButton.addEventListener("click", () => {
+    if (!isAnimating) {
+      closeDropdown(infosDropdown, () => {
+        placesDropdown.classList.toggle("open");
+      });
+    }
+  });
+});
