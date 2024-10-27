f0cf77099d58d24466e694c3280c27eba2eb72ff
jwojszczyk <jwojszczyk@gmail.com>
2024-10-27 15:48:09 +0100

    phone numbers height medical centers titles

--- index3.js
+++ index3.js
@@ -96,7 +96,6 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "porWie.html";
   });
 });
-//
 document.addEventListener("DOMContentLoaded", function () {
   // Get all dropdowns
   let dropdowns = document.querySelectorAll(".dropdown");
@@ -151,7 +150,7 @@ function handleButtonClick(buttonId) {
     bottom_menu_button1: "poradnie.html",
     bottom_menu_button2: "laboratoria.html",
     bottom_menu_button3: "medycyna.html",
-    bottom_menu_button4: "opieka.html",
+    bottom_menu_button4: "http://4.html",
   };
 
   window.location.href = pageMap[buttonId];
@@ -222,63 +221,3 @@ function moveToPrevSlide() {
     track.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
   }
 }
-window.addEventListener("scroll", function () {
-  var elements = document.querySelectorAll(".bottom-menu-tabs");
-
-  elements.forEach(function (element, index) {
-    var position = element.getBoundingClientRect();
-
-    // Sprawdza, czy element jest w widocznej części ekranu
-    if (position.top <= window.innerHeight && position.bottom >= 0) {
-      // Dodajemy opóźnienie do animacji
-      element.style.transitionDelay = index * 0.15 + "s";
-      element.classList.add("visible");
-    }
-  });
-});
-
-const menuBtn = document.querySelector(".menu-btn");
-const navMenu = document.querySelector(".nav-menu");
-
-menuBtn.addEventListener("click", () => {
-  menuBtn.classList.toggle("open");
-  navMenu.classList.toggle("activeH");
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  const infosButton = document.getElementById("infos_btn");
-  const infosDropdown = document.getElementById("infos_dropdown");
-  const placesButton = document.getElementById("places_btn");
-  const placesDropdown = document.getElementById("places_dropdown");
-
-  let isAnimating = false; // Zmienna do śledzenia animacji
-
-  function closeDropdown(dropdown, callback) {
-    if (dropdown.classList.contains("open")) {
-      isAnimating = true; // Ustawiamy animację na true
-      dropdown.classList.remove("open");
-      setTimeout(() => {
-        isAnimating = false; // Po zakończeniu animacji ustawiamy na false
-        callback();
-      }, 300); // Ustaw czas zgodny z transition
-    } else {
-      callback();
-    }
-  }
-
-  infosButton.addEventListener("click", () => {
-    if (!isAnimating) {
-      closeDropdown(placesDropdown, () => {
-        infosDropdown.classList.toggle("open");
-      });
-    }
-  });
-
-  placesButton.addEventListener("click", () => {
-    if (!isAnimating) {
-      closeDropdown(infosDropdown, () => {
-        placesDropdown.classList.toggle("open");
-      });
-    }
-  });
-});
