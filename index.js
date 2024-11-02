66e244200b255fd527150f67639b0d3732ef8863
jwojszczyk <jwojszczyk@gmail.com>
2024-11-02 17:59:34 +0100

    modal on index added

--- index.js
+++ index.js
@@ -162,6 +162,29 @@ window.onload = function () {
   }
 };
 
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("contact-modal");
+  const contactButton = document.getElementById("contactButton");
+  const closeButton = document.querySelector(".close-button");
+
+  // Open modal when button is clicked
+  contactButton.addEventListener("click", () => {
+    modal.style.display = "flex";
+  });
+
+  // Close modal when clicking on the close button
+  closeButton.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  // Close modal when clicking outside the modal content
+  window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
+
 function handleButtonClick(buttonId) {
   const pageMap = {
     bottom_menu_button1: "poradnie.html",
